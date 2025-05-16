91639c457a0645813242ffc09a05f37e324da2ec
Nahid Khan <nahidkhanniloy31@gmail.com>
2025-05-17 01:56:01 +0600

    added first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("email server is working...");
+});
+
+app.listen(port, () => {
+  console.log(`email server is working... ${port}`);
+});
