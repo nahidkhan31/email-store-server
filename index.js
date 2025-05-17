fb81a521091e17c56f7fb63b44c889584000a8ad
Nahid Khan <nahidkhanniloy31@gmail.com>
2025-05-17 19:21:15 +0600

    added code

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 3000;
@@ -27,10 +27,47 @@ async function run() {
 
     const emailCollection = client.db("emailDB").collection("emails");
 
+    // email data ke client e pathanor jonno....
+    app.get("/emails", async (req, res) => {
+      const result = await emailCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/emails/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await emailCollection.findOne(query);
+      res.send(result);
+    });
+
     app.post("/emails", async (req, res) => {
       const newEmail = req.body;
       console.log(newEmail);
       const result = await emailCollection.insertOne(newEmail);
+      res.send(result);
+    });
+
+    app.put("/emails/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedEmail = req.body;
+      const updatedDoc = {
+        $set: updatedEmail,
+      };
+
+      const result = await emailCollection.updateOne(
+        filter,
+        updatedDoc,
+        options
+      );
+      res.send(result);
+    });
+
+    app.delete("/emails/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await emailCollection.deleteOne(query);
       res.send(result);
     });
 
